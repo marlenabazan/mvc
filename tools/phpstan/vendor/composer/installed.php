c24828797947d4ca2e69599d3d620b68809a43e9
Marlena Bazan <marlena.bazan@hotmail.com>
2022-11-05 01:24:49 +0100

    Added phpstan

---
+++ tools/phpstan/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd8c89e97fbed6a485d0b8780255430d75759bc91',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd8c89e97fbed6a485d0b8780255430d75759bc91',
+            'dev_requirement' => false,
+        ),
+        'phpstan/phpstan' => array(
+            'pretty_version' => '1.9.1',
+            'version' => '1.9.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'reference' => 'a59c8b5bfd4a236f27efc8b5ce72c313c2b54b5f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
