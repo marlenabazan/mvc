1a56e36e844daa2f65fe281cfd64aa57a0c7463d
Marlena Bazan <marlena.bazan@hotmail.com>
2022-11-09 00:50:53 +0100

    Tests Player

---
+++ tests/Game/PlayerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Card;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test cases for class Player.
+ */
+class PlayerTest extends TestCase
+{
+    /**
+     * Construct player object
+     * 
+     */
+    public function testCreatePlayer()
+    {
+        $player = new Player();
+        $this->assertInstanceOf("\App\Card\Player", $player);
+    }
+
+    /**
+     * Test get players hand
+     * 
+     */
+    public function testGetPlayersHand()
+    {
+        $player = new Player();
+
+        $res = $player->getPlayersHand();
+        $this->assertInstanceOf("\App\Card\Hand", $res);
+    }
+}
