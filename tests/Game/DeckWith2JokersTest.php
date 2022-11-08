7de0a19f5b0c0f096de47cd072ac9e3d6f734c55
Marlena Bazan <marlena.bazan@hotmail.com>
2022-11-09 00:12:15 +0100

    Tests DeckWith2Jokers

---
+++ tests/Game/DeckWith2JokersTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Card;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test cases for class DeckWith2Jokers.
+ */
+class DeckWith2JokersTest extends TestCase
+{
+    /**
+     *  DeckWith2Jokers object
+     * 
+     */
+    public function testCreateDeckWith2Jokers()
+    {
+        $deck = new \App\Card\DeckWith2Jokers();
+        $this->assertInstanceOf("\App\Card\Card", $deck);
+
+        $res = $deck->getDeckWithJokers();
+        $this->assertEquals(end($res), 'joker2');
+    }
+}
