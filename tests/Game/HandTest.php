8c066bbd5f7a2f053c75453598b5b1aa17a1b8d9
Marlena Bazan <marlena.bazan@hotmail.com>
2022-11-09 00:43:31 +0100

    Tests Hand

---
+++ tests/Game/HandTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Card;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test cases for class Hand.
+ */
+class HandTest extends TestCase
+{
+    /**
+     * Construct hand object
+     * 
+     */
+    public function testCreateHand()
+    {
+        $hand = new Hand();
+        $this->assertInstanceOf("\App\Card\Hand", $hand);
+    }
+
+    /**
+     * Test add card to hand
+     * 
+     */
+    public function testAddCard()
+    {
+        $hand = new Hand();
+        $hand->addCard('6H');
+
+        $this->assertEquals($hand->hand[0], '6H');
+    }
+}
